7828992260b72285510b2593d4930ddd95ac52db
Piyush24102000 <piyush20001024@gmail.com>
2023-01-03 18:44:59 +0530

    Controller and Router Update by Piyush

--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,6 @@ router.post('/authors',controller.createAuthor)
 router.post('/blogs',controller.createBlog)
 router.get('/blogs',controller.getBlog)
 router.put('/blogs/:blogId',controller.updateBlog)
+router.delete('/blogs/:blogId',controller.deleteParam)
+router.delete('/blogs',controller.deleteQuery)
 module.exports = router
