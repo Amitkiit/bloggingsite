66871a0682b2443e3b255db89be92d0889f669ae
Amitkiit <116937662+Amitkiit@users.noreply.github.com>
2023-01-07 09:40:23 +0530

    Update authorization.js

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -7,7 +7,7 @@ const authorization = async function (req, res, next) {
         //console.log(x)
         let Id = req.Id
         //console.log(Id)
-        let db = await blogSchema.findOne({_Id:x})
+        let db = await blogSchema.findOne({_id:x})
         //console.log(db)
         if (!db) return res.status(404).send({ msg: "data not found" })
         let y = db.authorId.toString()
